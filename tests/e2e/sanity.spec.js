648e22428e62ed4fa3db8511abb26f851afe41a3
Your Name <github.com@landroverz.com>
2022-06-11 15:51:24 +0300

    added workflow

--- tests/e2e/sanity.spec.js
+++ tests/e2e/sanity.spec.js
@@ -3,12 +3,11 @@ const {  scrape, parse } = require('../../src/scrapa');
 const dataUrls = {
     xml: 'https://raw.githubusercontent.com/LandRover/scrapa/master/tests/fixtures/books_list.xml',
     json: 'https://raw.githubusercontent.com/LandRover/scrapa/master/tests/fixtures/books_list.json',
-    html: 'https://www.w3schools.com/js/js_json_parse.asp',
+    html: 'https://raw.githubusercontent.com/LandRover/scrapa/master/tests/fixtures/html_simple_list.html',
 };
 
 // 'When no price is specified, then the product status is pending approval'
 describe('Sanity', function() {
-
 
     describe('Handle XML', function () {
         let url = dataUrls.xml;
@@ -44,14 +43,12 @@ describe('Sanity', function() {
     describe('Handle JSON', function () {
         let url = dataUrls.json;
 
-        it('When parsing an JSON user from github, then extract name, age, list and pets from seed url', async function () {
+        it('When parsing an JSON document from github, then extract author, title fields only', async function () {
             // Act
             let body = await scrape({
                 url,
                 type: 'get',
             });
-
-            console.log(['body', '"'+body+'"']);
 
             let sut = await parse({
                 body,
@@ -66,11 +63,45 @@ describe('Sanity', function() {
             
             expect(sut.total).toBe(12);
             expect(sut.total).toBe(sut.fields.length);
-            expect(sut.fields[0].author).toBe('Gambardella, Matthew');
-            expect(sut.fields[0].title).toBe("XML Developer's Guide");
-            expect(sut.fields[0].description).toBeUndefined();
+            expect(sut.fields[sut.fields.length-1].author).toBe('Galos, Mike');
+            expect(sut.fields[sut.fields.length-1].title).toBe('Visual Studio 7: A Comprehensive Guide');
+            expect(sut.fields[sut.fields.length-1].description).toBeUndefined();
         });
 
 
-    }); 
+    });
+
+
+    describe('Handle HTML', function () {
+        let url = dataUrls.html;
+
+        it('When parsing an HTML page from github, extract a UL element and return a list of drinks', async function () {
+            // Act
+            let body = await scrape({
+                url,
+                type: 'get',
+            });
+
+            let sut = await parse({
+                body,
+                type: 'html',
+                fields: {
+                    first: 'ul li.first',
+                    third: 'ul li.third',
+                },
+            });
+            
+            // Assert
+            
+            expect(sut.total).toBe(1);
+            expect(sut.total).toBe(sut.fields.length);
+            expect(sut.fields[sut.fields.length-1].first).toBe('Coffee');
+            expect(sut.fields[sut.fields.length-1].third).toBe('Milk');
+            expect(sut.fields[sut.fields.length-1].second).toBeUndefined();
+        });
+
+
+    });
+
+
 });
