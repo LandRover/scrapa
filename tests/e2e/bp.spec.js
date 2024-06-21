a23024201df838e3152cb3197f1591f14e132796
LandRover <github@landroverz.com>
2024-06-21 08:05:36 +0300

    added chart tests

--- tests/e2e/bp.spec.js
+++ tests/e2e/bp.spec.js
@@ -4,7 +4,7 @@ import { isValidURL } from '../helpers/is_valid_url.js';
 //
 describe('BP Top 100', function() {
 
-    describe('Extract JSON from BP, parse it and structure', function () {
+    describe('Extract JSON from BP, parse it and structure, top 100', function () {
         let URLbase64 = 'aHR0cHM6Ly93d3cuYmVhdHBvcnQuY29tL3RvcC0xMDA=';
         let URLRaw = new Buffer(URLbase64, 'base64').toString('ascii'); // decode
 
@@ -13,7 +13,7 @@ describe('BP Top 100', function() {
             let scrapeResponse = await scrape({
                 url: URLRaw,
                 type: 'get',
-                regExp: [new RegExp('<script id="__NEXT_DATA__" type="application/json">(.*?)</script>')],
+                regExp: [new RegExp('<script id="__NEXT_DATA__" type="application\/json">(.*)<\/script>')],
             });
 
             let sut = await parse({
@@ -29,7 +29,7 @@ describe('BP Top 100', function() {
                     price: 'props.pageProps.dehydratedState.queries.0.state.data.results.{Iterator}.price.value',
                     duration: 'props.pageProps.dehydratedState.queries.0.state.data.results.{Iterator}.length',
                     date: 'props.pageProps.dehydratedState.queries.0.state.data.results.{Iterator}.new_release_date',
-                    image: 'props.pageProps.dehydratedState.queries.0.state.data.results.{Iterator}.release.image.uri'
+                    image: 'props.pageProps.dehydratedState.queries.0.state.data.results.{Iterator}.release.image.uri',
                 },
             });
 
@@ -52,4 +52,52 @@ describe('BP Top 100', function() {
 
     });
 
+
+    describe('Extract JSON from BP, parse it and structure, psy top 100', function () {
+        let URLbase64 = 'aHR0cHM6Ly93d3cuYmVhdHBvcnQuY29tL2dlbnJlL3BzeS10cmFuY2UvMTMvdHJhY2tzP3BhZ2U9MSZwZXJfcGFnZT0xMDAmcHJlb3JkZXJzPWZhbHNl';
+        let URLRaw = new Buffer(URLbase64, 'base64').toString('ascii'); // decode
+
+        it('Extract chart BP data', async function () {
+            // Act
+            let scrapeResponse = await scrape({
+                url: URLRaw,
+                type: 'get',
+                regExp: [new RegExp('<script id="__NEXT_DATA__" type="application\/json">(.*)<\/script>')],
+            });
+
+            let sut = await parse({
+                body: scrapeResponse.body(),
+                type: 'json',
+                fields: {
+                    id: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.id',
+                    artist: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.artists.0.name',
+                    title: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.name',
+                    label: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.release.label.name',
+                    mix: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.mix_name',
+                    bpm: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.bpm',
+                    price: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.price.value',
+                    duration: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.length',
+                    date: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.new_release_date',
+                    image: 'props.pageProps.dehydratedState.queries.1.state.data.results.{Iterator}.release.image.uri',
+                },
+            });
+
+            // Assert
+            expect(sut.total).toBe(100);
+            expect(sut.total).toBe(sut.fields.length);
+
+            expect(sut.fields[0].id).toEqual(expect.any(Number))
+            expect(sut.fields[0].artist).not.toHaveLength(0);
+            expect(sut.fields[0].title).not.toHaveLength(0);
+            expect(sut.fields[0].label).not.toHaveLength(0);
+            expect(sut.fields[0].mix).not.toHaveLength(0);
+            expect(sut.fields[0].bpm).toEqual(expect.any(Number))
+            expect(sut.fields[0].price).toEqual(expect.any(Number))
+            expect(sut.fields[0].duration).not.toHaveLength(0);
+            expect(sut.fields[0].date).not.toHaveLength(0);
+            expect(isValidURL(sut.fields[0].image)).toBe(true);
+        });
+
+
+    });
 });
